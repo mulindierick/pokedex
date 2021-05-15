54a27df74477b16dd404f0bbb6b1518032b55dbb
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-05-15 10:36:10 +0300

    edited the app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Erick's app
+          Hi, welcome to react!
         </p>
         <a
           className="App-link"
